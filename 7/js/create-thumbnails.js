b91fef627843791d3f7fa9aeebd70071baa54f0e
Keks <keks@htmlacademy.ru>
2023-03-28 21:27:57 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/create-thumbnails.js
@@ -0,0 +1,22 @@
+import { createDescriptions } from './data.js';
+
+const allPictures = document.querySelector('.pictures');
+const usersPicturesTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+const usersPictures = createDescriptions();
+
+const usersPicturesFragment = document.createDocumentFragment();
+
+usersPictures.forEach(({url, description, likes, comments, id}) => {
+  const userElement = usersPicturesTemplate.cloneNode(true);
+  userElement.querySelector('.picture__img').src = url;
+  userElement.querySelector('.picture__img').alt = description;
+  userElement.querySelector('.picture__likes').textContent = likes;
+  userElement.querySelector('.picture__comments').textContent = comments.length;
+  userElement.dataset.index = id;
+  usersPicturesFragment.append(userElement);
+});
+
+allPictures.appendChild(usersPicturesFragment);
+
+export {usersPictures};
---
+++ 7/js/main.js
@@ -0,0 +1,3 @@
+import './create-thumbnails.js';
+import './showfullscreenpicture.js';
+import './createfullscreenpicture.js';
