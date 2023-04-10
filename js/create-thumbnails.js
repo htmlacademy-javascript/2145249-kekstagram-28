635abb5ba8b561a6c46d848556e5e8ab8c32411c
Sigrdriva <gavrilenko.m.e@gmail.com>
2023-04-10 22:09:57 +0300

    Добавляет работу фильтров фотографий

--- js/create-thumbnails.js
+++ js/create-thumbnails.js
@@ -1,18 +1,84 @@
 const allPictures = document.querySelector('.pictures');
 const picturesTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const defaultButton = document.querySelector('#filter-default');
+const randomButton = document.querySelector('#filter-random');
+const discussedButton = document.querySelector('#filter-discussed');
+const RANDOM_PHOTO_COUNT = 10;
+
+const showChosenFilter = (evt) => {
+  evt.target.classList.add('img-filters__button--active');
+};
+
+const hideUnchosenFilter = () => {
+  const allButtons = document.querySelectorAll('.img-filters__button');
+  allButtons.forEach((element)=>element.classList.remove('img-filters__button--active'));
+};
+
+
+let comparePictures = () => true;
+let photosCount;
+
+const defaultSort = (render, photosCountDefault) => {
+  defaultButton.addEventListener('click', (evt) => {
+    hideUnchosenFilter();
+    showChosenFilter(evt);
+    document.querySelectorAll('.picture').forEach((element)=>element.remove());
+    photosCount = photosCountDefault;
+    comparePictures = () => true;
+    render();
+  });
+};
+
+const randomSort = (render) => {
+  randomButton.addEventListener('click', (evt) => {
+    hideUnchosenFilter();
+    showChosenFilter(evt);
+    document.querySelectorAll('.picture').forEach((element)=>element.remove());
+    photosCount = RANDOM_PHOTO_COUNT;
+    comparePictures = () => (Math.random() - 0.5);
+    render();
+  });
+};
+
+const discussedSort = (render, photosCountDefault) => {
+  discussedButton.addEventListener('click', (evt) => {
+    hideUnchosenFilter();
+    showChosenFilter(evt);
+    document.querySelectorAll('.picture').forEach((element)=>element.remove());
+    photosCount = photosCountDefault;
+    comparePictures = (pictureA, pictureB) => {
+      const rankA = pictureA.comments.length;
+      const rankB = pictureB.comments.length;
+      return rankB - rankA;
+    };
+    render();
+  });
+};
+
+function debounce (callback, timeoutDelay = 500) {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+}
 
 const renderPictures = (pictures) => {
   const picturesFragment = document.createDocumentFragment();
-  pictures.forEach(({url, description, likes, comments, id}) => {
-    const userElement = picturesTemplate.cloneNode(true);
-    userElement.querySelector('.picture__img').src = url;
-    userElement.querySelector('.picture__img').alt = description;
-    userElement.querySelector('.picture__likes').textContent = likes;
-    userElement.querySelector('.picture__comments').textContent = comments.length;
-    userElement.dataset.index = id;
-    picturesFragment.append(userElement);
-  });
+  pictures
+    .slice()
+    .sort(comparePictures)
+    .slice(0, photosCount)
+    .forEach(({url, description, likes, comments, id}) => {
+      const userElement = picturesTemplate.cloneNode(true);
+      userElement.querySelector('.picture__img').src = url;
+      userElement.querySelector('.picture__img').alt = description;
+      userElement.querySelector('.picture__likes').textContent = likes;
+      userElement.querySelector('.picture__comments').textContent = comments.length;
+      userElement.dataset.index = id;
+      picturesFragment.append(userElement);
+    });
   allPictures.appendChild(picturesFragment);
 };
 
-export { renderPictures };
+export { renderPictures, debounce, defaultSort, randomSort, discussedSort };
--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
 import './create-fullscreen-picture.js';
-import {setUserFormSubmit, closePicture} from './form.js';
+import { setUserFormSubmit, closePicture } from './form.js';
 import './edit-new-photo.js';
-import {renderPictures} from './create-thumbnails.js';
-import {setGalleryHandlers} from './show-fullscreen-picture.js';
-import {getData} from './api.js';
+import { renderPictures, debounce, defaultSort, randomSort, discussedSort } from './create-thumbnails.js';
+import { setGalleryHandlers } from './show-fullscreen-picture.js';
+import { getData } from './api.js';
 import { showAlert } from './util.js';
 
 getData()
   .then((photos) => {
     renderPictures(photos);
+    defaultSort(debounce(()=> renderPictures(photos), photos.length));
+    randomSort(debounce(() => renderPictures(photos)));
+    discussedSort(debounce(() => renderPictures(photos), photos.length));
     setGalleryHandlers(photos);
+    document.querySelector('.img-filters').classList.remove('img-filters--inactive');
   })
   .catch((err) => {
     showAlert(err);
